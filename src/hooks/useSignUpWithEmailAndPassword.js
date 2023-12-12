690a7e1579037f4c3326abd93c1057f06352eb38
Neshad <djneshad@gmail.com>
2023-12-12 22:16:22 +0600

    getting fckn errors

--- src/hooks/useSignUpWithEmailAndPassword.js
+++ src/hooks/useSignUpWithEmailAndPassword.js
@@ -1,26 +1,28 @@
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { auth } from "../firebase/firebase"
+import { auth, firestore } from "../firebase/firebase"
+import { doc, setDoc } from 'firebase/firestore';
+import useShowToast from './useShowToast';
 
 const useSignUpWithEmailAndPassword = () => {
   const [
     createUserWithEmailAndPassword,
-    user,
     loading,
     error,
   ] = useCreateUserWithEmailAndPassword(auth);
+  const showToast = useShowToast();
 
   const signup = async (inputs) => {
-    if (!inputs.email || !inputs.password || inputs.fullname) {
-      console.log("Please fill all the fields")
-      return
+    if (!inputs.email || !inputs.password || !inputs.username || !inputs.fullname) {
+      showToast("Error", "Please fill all the fields", "error")
+      return;
     }
     try {
       const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password)
       if (!newUser && error) {
-        console.log(error)
-        return
+        showToast("Error", error.errors.message, "error");
+        return;
       }
-      if (newUser{
+      if (newUser) {
         const userDoc = {
           uid: newUser.user.uid,
           email: inputs.email,
@@ -31,15 +33,16 @@ const useSignUpWithEmailAndPassword = () => {
           followers: [],
           following: [],
           posts: [],
-          createdAt: Date.now()
-        }
-
-      })
+          createdAt: Date.now(),
+        };
+        await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
+        localStorage.setItem("user-info", JSON.stringify(userDoc))
+      }
     }
     catch (error) {
-      console.log(error)
+      showToast("Error", error.message, "error")
     }
-  }
+  };
 
   return { loading, error, signup }
 
