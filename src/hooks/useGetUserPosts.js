f27548baba3548289717c628af8e143e61f6e178
Neshad <djneshad@gmail.com>
2024-01-08 21:46:42 +0600

    working

--- src/hooks/useGetUserPosts.js
+++ src/hooks/useGetUserPosts.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import usePostStore from "../store/postStore";
 import useShowToast from "./useShowToast";
 import useUserProfileStore from "../store/userProfileStore";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { firestore } from "../firebase/firebase";
 
 const useGetUserPosts = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -15,6 +17,16 @@ const useGetUserPosts = () => {
       setIsLoading(true);
       setPosts([])
       try {
+        const q = query(collection(firestore, "posts"), where("createdBy", "==", userProfile.uid));
+        const querySnapshot = await getDocs(q);
+        const posts = []
+        querySnapshot.forEach(doc => {
+          posts.push({ ...doc.data(), id: doc.id })
+        })
+
+        //latest post appears on top
+        posts.sort((a, b) => b.createdAt - a.createdAt)
+        setPosts(posts);
 
       } catch (error) {
         showToast("Error", error.message, "error")
@@ -22,8 +34,11 @@ const useGetUserPosts = () => {
       } finally {
         setIsLoading(false);
       }
-    }
-  }, [])
+    };
+    getPosts();
+  }, [setPosts, userProfile, showToast])
+
+  return { isLoading, posts }
 
 }
 
